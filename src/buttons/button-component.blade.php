a5312e43f3f21f7b867bfd85a095d01a934fd918
Giuliano Sampaio <giuguitar@gmail.com>
2024-10-02 12:07:47 -0300

    id button

--- src/buttons/button-component.blade.php
+++ src/buttons/button-component.blade.php
@@ -1,6 +1,7 @@
 <button 
     @if(isset($click)) wire:click="{{ $click }}" @endif
     tabindex="{{ $tabindex ?? '0'}}"
+    id="{{ $id ?? 'btn' }}"
     wire:loading.class="disabled btn-progress"
     class="btn {{ isset($class) ? 'btn-' . $class : '' }} ">
         {{ $slot }}
