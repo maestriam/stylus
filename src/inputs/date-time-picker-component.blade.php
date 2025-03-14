066100a889c12c172b82b579c81f9d6caea5e826
Giuliano Sampaio <giuguitar@gmail.com>
2025-03-14 10:45:16 -0300

    validation datetimepicker

--- src/inputs/date-time-picker-component.blade.php
+++ src/inputs/date-time-picker-component.blade.php
@@ -4,21 +4,26 @@
 
 <input 
     type="text" 
-    readonly
     wire:model="{{ $model }}"
     data-livewire="@this"
     value="{{ $value ?? '' }}"
     wire.loading.attr="readonly"
     tabindex="{{ $tabindex ?? '1' }}" 
-    class="form-control datetimepicker"
-    id="{{ isset($name) ? $name .'-id' : 'date-time-picker-id' }}" 
-    name="{{ $name ?? 'date-time-picker' }}"   
-         
+    class="form-control datetimepicker 
+    {{ $errors->has($name ?? 'input') ? 'is-invalid' : '' }}"
+    name="{{ $name ?? 'date-time-picker' }}"
     @if (isset($required)) required @endif
     @if (isset($autofocus)) autofocus @endif
+    id="{{ isset($name) ? $name .'-id' : 'date-time-picker-id' }}" 
     @if (isset($readonly) && $readonly == true) readonly @endif  
-    @if (isset($enter)) {{ 'wire:keydown.enter=' . $enter }} @endif
-/>
+    @if (isset($enter)) {{ 'wire:keydown.enter=' . $enter }} @endif    
+    />
+
+<!-- error message -->
+@error($name ?? 'input')
+<div class="text-danger">{{ $message }}</div>
+@enderror
+<!-- /error message -->
 
 @pushOnce('scripts')
 <script type="text/javascript">
@@ -26,10 +31,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // @todo: verificar uma forma mais limpa de receber 
     // os dados do datetimepicket
+    
     $('.datetimepicker').on('apply.daterangepicker', function() {
         let prop = $(this).attr('wire:model')
         @this.set(prop, $(this).val())
     });
+    
+    $('.datetimepicker').on('hide.daterangepicker', function() {
+        let prop = $(this).attr('wire:model')
+        @this.set(prop, $(this).val())
+    }); 
 
 });
 </script>
