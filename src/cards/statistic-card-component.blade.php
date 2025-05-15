78f8e4084324c13cc9cd20b471a2dfc9b98e4e02
Giuliano Sampaio <giuguitar@gmail.com>
2025-05-15 09:19:02 -0300

    statistic card

---
+++ src/cards/statistic-card-component.blade.php
@@ -0,0 +1,15 @@
+<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+    <div class="card card-statistic-1">
+        <div class="card-icon bg-warning">
+            <i class="far {{ isset($icon) ? 'fa-' . $icon : 'fa-clock' }}"></i>
+        </div>
+        <div class="card-wrap">
+            <div class="card-header">
+                <h4>{{ $label }}</h4>
+            </div>
+            <div class="card-body">
+                {{ $value }}
+            </div>
+        </div>
+    </div>
+</div>
--- src/form/form-control-label-component.blade.php
+++ src/form/form-control-label-component.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group row align-items-{{$align ?? 'center' }} {{ $mb ?? 'mb-1' }}">
-    <label class="form-control-label col-sm-3 text-md-right">{{ $label }}</label>
-    <div class="col-sm-6 col-md-9">
+    <label class="form-control-label col-sm-3 col-md-6 col-lg-4 text-md-right">{{ $label }}</label>
+    <div class="col-sm-6 col-md-6 col-lg-8">
         <label>{{ $slot }}</label>
     </div>
 </div>
