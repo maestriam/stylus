f7436079c3f8551cf31cd65d113be4e8d4297022
Giuliano Sampaio <giuguitar@gmail.com>
2025-06-25 00:44:43 -0300

    profile widget modification

--- src/widget/profile-widget-component.blade.php
+++ src/widget/profile-widget-component.blade.php
@@ -11,6 +11,38 @@
                 <a class="text-primary">{{ $name ?? '' }}</a>
             </div>
             <div class="author-box-job">{{ $account ?? '' }}</div>
+        </div>        
+    </div>
+    @if( isset($createdAt) || isset($email) )
+    <div class="card-body p-0">
+        <div class="row">
+            @isset($email)
+            <div class="col-2">
+                <div class="tickets-list">
+                    <span href="#" class="ticket-item">                    
+                        <div class="ticket-info">
+                            <div>E-mail</div>
+                            <div class="bullet"></div>
+                            <label class="text-primary">{{ $email }}</label>
+                        </div>
+                    </span>
+                </div>
+            </div>
+            @endisset
+            @isset($createdAt)
+            <div class="col-3">
+                <div class="tickets-list">
+                    <span href="#" class="ticket-item">                    
+                        <div class="ticket-info">
+                            <div>Criado em:</div>
+                            <div class="bullet"></div>
+                            <label>{{ $createdAt }}</label>
+                        </div>
+                    </span>
+                </div>
+            </div>
+            @endisset
         </div>
     </div>
+    @endif
 </div>
